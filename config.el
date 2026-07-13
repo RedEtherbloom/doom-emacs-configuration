38b8c8769390fb81ddfd3200a218eb0987629566
RedEtherbloom <etherbloom@mailbox.org>
2026-07-13 02:21:12 +0200

    feat: install calibredb

--- config.el
+++ config.el
@@ -21,8 +21,8 @@
 ;; See 'C-h v doom-font' for documentation and more examples of what they
 ;; accept. For example:
 ;;
-(setq doom-font (font-spec :family "FiraCode Nerd Font" :size 12 :weight 'semi-light)
-     doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+(setq doom-font (font-spec :family "FiraCode Nerd Font" :size 14 :weight 'bold)
+     doom-variable-pitch-font (font-spec :family "Fira Sans" :size 15))
 ;;
 ;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
@@ -32,7 +32,7 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-tokyo-night)
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -80,3 +80,16 @@
 ;; Use my own shell for terminals 
 (setq-default vterm-shell "/run/current-system/sw/bin/fish")
 (setq-default explicit-shell-file-name "/run/current-system/sw/bin/fish")
+
+;; TODO: Gets overwritten by others, sigh
+(setq-default tab-width 2)
+(setq-default indent-tabs-mode 's)
+
+;; TODO: Hooks don't get triggerd
+;; (setq-hook! 'rustic-mode-hook lsp-inlay-hints-enable true)
+(add-hook! 'rustic-mode-hook (setq lsp-inlay-hints-enable t))
+
+(use-package! calibredb
+  :commands calibredb calibredb-list calibredb-find-file calibredb-find-helm
+  :config
+  (setq calibredb-root-dir "~/Documents/Calibre Library"))
